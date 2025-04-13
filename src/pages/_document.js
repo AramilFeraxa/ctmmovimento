40250a6d75ee4a0aa34ab64d17f369488e8feb75
Mateusz Kopeć <lordvader1505@gmail.com>
2025-04-13 16:27:55 +0200

    url fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,12 +6,12 @@ class MyDocument extends Document {
         return (
             <Html lang="pl-PL">
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png" />
-                    <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png" />
+                    <link rel="icon" href="./favicon.ico" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+                    <link rel="android-chrome" sizes="192x192" href="./android-chrome-192x192.png" />
+                    <link rel="android-chrome" sizes="512x512" href="./android-chrome-512x512.png" />
 
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
