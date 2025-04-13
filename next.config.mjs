4323d315593fb25556ae2fcfb221d6b6a72aff69
AramilFeraxa <aferaxa@gmail.com>
2025-04-13 14:33:21 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/ctmmovimento',
+    assetPrefix: '/ctmmovimento/',
+    compiler: {
+        styledComponents: true
+    },
+    reactStrictMode: true,
+    trailingSlash: true,
+    output: 'export',
+};
 
 export default nextConfig;
--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -5,9 +5,6 @@ export default function Gallery() {
         <section className={styles.gallery}>
             <h2>Galeria</h2>
             <div className={styles.grid}>
-                {[1, 2, 3, 4, 5, 6].map(i => (
-                    <div key={i} className={styles.item} style={{ backgroundImage: `url(/images/gallery${i}.jpg)` }} />
-                ))}
             </div>
         </section>
     )
